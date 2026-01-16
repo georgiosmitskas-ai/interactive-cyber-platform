93f12b99b2e58bc15b2c1565c84a117802707795
Georgios Mitskas <georgios.mitskas@gmail.com>
2026-01-16 09:28:34 +0000

    Build Vite app to docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// IMPORTANT: 'base' must match your repo name
+// and 'outDir' must be 'docs' for GitHub Pages.
 export default defineConfig({
   base: '/interactive-cyber-platform/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'docs'
+  }
 })
